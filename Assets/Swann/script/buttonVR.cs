5f1a85cfd844869272d92c16e4e77c7cff8c6b6f
OshidaBCF <swann.sanchez9@outlook.fr>
2024-03-13 17:58:13 +0100

    idk
    subnautica's building system

--- Assets/Swann/script/buttonVR.cs
+++ Assets/Swann/script/buttonVR.cs
@@ -11,6 +11,10 @@ public class buttonVR : MonoBehaviour
     bool wasReleased;
     GameObject button;
     GameObject presser;
+#pragma warning disable CS0108 // Deprecated warning
+    Rigidbody rigidbody;
+#pragma warning restore CS0108 // Deprecated warning
+    Vector3 buttonUp;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +22,29 @@ public class buttonVR : MonoBehaviour
         button = gameObject.transform.GetChild(0).gameObject;
         isPressed = false;
         wasReleased = true;
+
+        rigidbody = button.GetComponent<Rigidbody>();
+        buttonUp = button.transform.up;
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        if (Mathf.Round(buttonUp.x * 1000) == 0)
+            rigidbody.constraints |= RigidbodyConstraints.FreezePositionX;
+        if (Mathf.Round(buttonUp.y * 1000) == 0)
+            rigidbody.constraints |= RigidbodyConstraints.FreezePositionY;
+        if (Mathf.Round(buttonUp.z * 1000) == 0)
+            rigidbody.constraints |= RigidbodyConstraints.FreezePositionZ;
     }
 
     private void Update()
     {
         if (button.transform.localPosition.y < 2.5f)
             button.transform.localPosition = new Vector3(0, 2.5f, 0);
-
         if (button.transform.localPosition.y > 4f)
             button.transform.localPosition = new Vector3(0, 4f, 0);
+
+        button.transform.localPosition = new Vector3(0, button.transform.localPosition.y, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Enter");
         if (!wasReleased)
             return;
 
@@ -58,7 +72,6 @@ public class buttonVR : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Exit");
         if (!isButtonToggle && other.gameObject == presser)
         {
             onRelease.Invoke();
